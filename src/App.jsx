d6e9befacdae8be484268673179395c622326f68
Umang Raj <umangraj7725@gmail.com>
2026-03-21 23:22:00 +0530

    Static part of splitBill

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Children, useState } from "react";
 import { initialFriends } from "./Constants";
 
 export default function App() {
@@ -8,7 +8,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -36,6 +39,40 @@ function Friend({ friend }) {
   ) : (
     <p>You and {friend.name} are even</p>
   ) } 
-  <button className="button">Select</button>
+  <Button>Select</Button>
   </li>;
+}
+
+function Button({ children }) {
+  return (<button className="button">{children}</button>)
+}
+
+function FormAddFriend() {
+  return (
+  <form className="form-add-friend">
+    <label>👫 Friend Name</label>
+    <input type="text" />
+    <label>🌆 Image URL</label>
+    <input type="text" />
+  </form>
+  )
+}
+
+function FormSplitBill() {
+  return(
+  <form className="form-split-bill">
+   <h2>Split a bill with X</h2>
+    <label>💰 Bill Value</label>
+    <input type="text" />
+    <label>🧍 Your Expense</label>
+    <input type="text" />
+    <label>👫 {`X's Expense`}</label>
+    <input type="text" disabled />
+    <label>💵 Who is paying the bill?</label>
+    <select>
+      <option value="you">You</option>
+      <option value="friend">X</option>
+    </select>
+  </form>
+  )
 }
