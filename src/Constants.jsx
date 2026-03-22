0abe175f4018da1d149f0b84087ff9d283d91a59
Umang Raj <umangraj7725@gmail.com>
2026-03-22 14:46:34 +0530

    Made css dynamic and interactive

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,19 +1,19 @@
 export const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Umang",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Rahul",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Sumit",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
